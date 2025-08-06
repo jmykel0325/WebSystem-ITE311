3cc0727c2d6522a275bf40100fd234f911eaa5b7
John Michael Castillo <jmykel0325@gmail.com>
2025-08-06 14:29:27 +0800

    Added Bootstrap template and Home controller

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,2 +1,2 @@
 <h1 class="display-4">Welcome to ITE311 Web System</h1>
-<p class="lead">This is your homepage using CodeIgniter and Bootstrap.</p>
+<p class="lead">This is My homepage after using CodeIgniter and Bootstrap.</p>
